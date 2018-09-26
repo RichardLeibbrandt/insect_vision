790ed402c552a8eb5c582e003df94989a077951e
RichardLeibbrandt <richard.leibbrandt@flinders.edu.au>
2018-09-26 14:27:35 +0930

    Modified convert script that converts movie files into .mat sequences. Trivial (non-functional) changes in Starfield.

--- Stimuli/MatSequence/convert.m
+++ Stimuli/MatSequence/convert.m
@@ -1,14 +1,14 @@
 function convert(infile, outfile)
     v = VideoReader(infile);
     vidFrame = readFrame(v);
-    out = zeros(size(vidFrame,1), size(vidFrame,2), 3);
-    mcframe = rgb2gray(vidFrame);
+    out = zeros(size(vidFrame,1), size(vidFrame,2), 1);
+    mcframe = double(rgb2gray(vidFrame))/255;
     out(:,:,1) = mcframe;
     counter = 1;
     while hasFrame(v)
         counter = counter + 1;
         vidFrame = readFrame(v);
-        mcframe = rgb2gray(vidFrame);
+        mcframe = double(rgb2gray(vidFrame))/255;
         out(:,:,counter) = mcframe;
     end
     save(outfile, 'out');
